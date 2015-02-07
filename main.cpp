5444b24150d661dc7839d1d6ea9ac002271664c4
Vojtech <cima.vojtech@gmail.com>
2015-02-07 17:37:31 +0100

    structure

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include "main.h"
+#define DEBUG
 
 
-Mat loadDoubleImage(const char* filename)
-{
+Mat loadDoubleImage(const char* filename) {
     /*
     Returns double<0,1> Mat object loaded from the file
     */
@@ -15,8 +15,7 @@ Mat loadDoubleImage(const char* filename)
     return imageDouble;
 }
 
-void printMatdouble(Mat img)
-{
+void printMatdouble(Mat img) {
     /*
     Prints Mat double object values to std output
     */
@@ -30,13 +29,10 @@ void printMatdouble(Mat img)
 }
 
 
-void diffuse(vector<vector<double > > &data, double *eigVal)
-{
+void diffuse(vector<vector<double > > &data, double *eigVal) {
     /*
         DRAFT
     */
-
-
     double alpha = 10000.0;
     printf("Using diffuse coords...\n");
     for(size_t i = 0; i < data.size();i++)
@@ -50,40 +46,31 @@ void diffuse(vector<vector<double > > &data, double *eigVal)
 }
 
 
-vector<vector<double > > getDataPoints(double* eigVec, int nev, int vecLen)
-{
+vector<vector<double > > getDataPoints(double* eigVec, int nev, int vecLen) {
     /*
-  
         Create data points from k eigenvectors x stacked in to the COLUMNS M=[x0,...,xk]
-        Data points created from the rows of M and normalized ex dataPoint[0] - first k-dimensional point for clustering
-        
+        Data points created from the rows of M and normalized ex dataPoint[0] - first k-dimensional point for clustering    
     */
     vector<vector<double > > dataPoints(vecLen, vector<double> (nev));
 
-    for(int i = 0; i < nev; i++)
-    {
-        for(int j = 0; j<vecLen; j++)
-        {
+    for(int i = 0; i < nev; i++) {
+        for(int j = 0; j<vecLen; j++) {
             dataPoints[j][i] = eigVec[j+i*vecLen];
         }
     }
     
-
     return dataPoints;
 }
 
 
-vector<vector<double > > getDataPointsTrans(double* eigVec, int nev, int vecLen)
-{
+vector<vector<double > > getDataPointsTrans(double* eigVec, int nev, int vecLen) {
     /*
         Create data points from k eigenvectors x stacked in to the ROWS       
     */
     vector<vector<double > > dataPoints(nev, vector<double> (vecLen));
 
-    for(int i = 0; i < nev; i++)
-    {
-        for(int j = 0; j<vecLen; j++)
-        {
+    for(int i = 0; i < nev; i++) {
+        for(int j = 0; j<vecLen; j++) {
             dataPoints[i][j] = eigVec[j+i*vecLen];
         }
     }
@@ -92,17 +79,15 @@ vector<vector<double > > getDataPointsTrans(double* eigVec, int nev, int vecLen)
 }
 
 
-vector<vector<double > > spectralDecomposition(CSCMat mat, int nev)
-{
+vector<vector<double > > spectralDecomposition(CSCMat mat, int nev) {
     /*
-
     Compute Eigenvectors and Eigenvalues of lower Laplacian matrix 
 
     WHAT IS PROBLEM DIMENSION?
     WHAT IS MAXIMUM NUMBER EIGEN VALUES ?!?!
-
     */
     
+
     int n = mat.pCol.size() -1;           // dimension (number of rows or cols od similiar matrix)
     //int nev = 3;      //number of requested eigenvectors/eigenvalues
     int nconv;       // number of "converged" eigenvalues.
@@ -113,10 +98,20 @@ vector<vector<double > > spectralDecomposition(CSCMat mat, int nev)
 
     char uplo = 'L';
     
+    #ifdef DEBUG
+        printf("Spectral decomposition started \n");
+	unsigned int start_time = time(NULL);
+    #endif
+
     nconv = AREig(eigVal, eigVec, n, nnz, &mat.val[0], &mat.iRow[0], &mat.pCol[0], uplo, nev, "SM", 0, 0.0, 1000000);
 
+    #ifdef DEBUG
+        unsigned int end_time = time(NULL);
+        printf("Spectral decomposition finished in %u s\n", end_time-start_time);
+    #endif
+    
     Solution(nconv, n, nnz, &mat.val[0], &mat.iRow[0], &mat.pCol[0], uplo, eigVal, eigVec);
-  
+ 
     
     vector<vector<double > > dataPoints = getDataPoints(eigVec,nev,n); // unnormalized data point for clustering eigenvectors in columns
 
@@ -133,8 +128,7 @@ vector<vector<double > > spectralDecomposition(CSCMat mat, int nev)
 
 
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     int nev = atoi(argv[2]); // number of required EigenVal/Vecs
 
     printf("nev= %d\n",nev);
@@ -167,10 +161,8 @@ int main(int argc, char** argv)
         vector<int > clusters = meanshiftFast(dataPoints,sourceImage.rows,sourceImage.cols);
 
         
-        for(int i = 0; i<segmentedImage.rows; i++)
-        {
-            for(int j = 0; j< segmentedImage.cols; j++)
-            {       
+        for(int i = 0; i<segmentedImage.rows; i++) {
+            for(int j = 0; j< segmentedImage.cols; j++) {       
                 segmentedImage.at<double>(i,j) = ((clusters[j+i*segmentedImage.cols]*40)%256)/255.0;
             }
         }
@@ -186,7 +178,7 @@ int main(int argc, char** argv)
 
         
 
-        imwrite(./out.png,segmentedImage);
+        imwrite("./out.png",segmentedImage);
     //}
 
     printf("Done!\n");
